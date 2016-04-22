86a47da27183aa11df0df002d4e91c8bb00ea2da
jerry54604 <i_54604@hotmail.com>
2016-04-22 23:10:56 +0800

    Login integration with service

--- js/services.js
+++ js/services.js
@@ -4,7 +4,7 @@ app.factory('User', [function(){
   return {
     getUsername: function () {
       // TO-DO: Need to get from session instead of hardcode
-      return 'TestUser';
+      return window.localStorage.getItem("username"); //'TestUser';
     }
   };
 }])
@@ -13,18 +13,29 @@ app.service('BlankService', [function(){
 
 }]);
 
-app.service('LoginService', function($q) {
+app.service('LoginService', function($q, $http, $httpParamSerializerJQLike) {
   return {
-    loginUser: function(name, pw) {
+    loginUser: function(username, pass) {
       var deferred = $q.defer();
       var promise = deferred.promise;
       
-      // Change this line to get from service
-      if (name == 'user' && pw == 'secret') {
-        deferred.resolve('Welcome ' + name + '!');
-      } else {
-        deferred.reject('Wrong credentials.');
-      }
+      $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+      var link = 'http://localhost/eat4free/login.php';
+      
+      $http({
+        method: 'POST',
+        url: link,
+        data: $httpParamSerializerJQLike({username : username, pass: pass}),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function successCallback(response) {
+          window.localStorage.setItem('username', username);
+          window.localStorage.setItem('Auth', response.data);
+          
+          deferred.resolve('Welcome ' + username + '!');
+        }, function errorCallback(response) {
+          deferred.reject('Wrong credentials.');
+        });
+      
       promise.success = function(fn) {
         promise.then(fn);
         return promise;
